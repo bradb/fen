021721911cc6514412a16d10204c46d0cfecfcde
Brad Bollenbach <bradbollenbach@riverford.co.uk>
2022-02-19 08:37:55 +0000

    more map-to-fen tests

--- test/core_test.clj
+++ test/core_test.clj
@@ -31,16 +31,6 @@
     (is (true? allow-black-queenside-castle?))
     (is (nil? en-passant-target-square))))
 
-(comment
-  (partition 8 [\r \n \b \q \k \b \n \r
-                \p \p \p \p \p \p \p \p
-                \- \- \- \- \- \- \- \-
-                \- \- \- \- \- \- \- \-
-                \- \- \- \- \- \- \- \-
-                \- \- \- \- \- \- \- \-
-                \P \P \P \P \P \P \P \P
-                \R \N \B \Q \K \B \N \R]))
-
 (deftest start-position-map-to-fen-test
   (let [m {:fen/board [\r \n \b \q \k \b \n \r
                        \p \p \p \p \p \p \p \p
@@ -91,6 +81,26 @@
     (is (= "e3" en-passant-target-square))
     ))
 
+(deftest after-1-e4-map-to-fen-test
+  (let [m {:fen/board [\r \n \b \q \k \b \n \r
+                       \p \p \p \p \p \p \p \p
+                       \- \- \- \- \- \- \- \-
+                       \- \- \- \- \- \- \- \-
+                       \- \- \- \- \P \- \- \-
+                       \- \- \- \- \- \- \- \-
+                       \P \P \P \P \- \P \P \P
+                       \R \N \B \Q \K \B \N \R]
+           :fen/side-to-move :black
+           :fen/fullmove-number 1
+           :fen/halfmove-clock 0
+           :fen/allow-white-kingside-castle? true
+           :fen/allow-white-queenside-castle? true
+           :fen/allow-black-kingside-castle? true
+           :fen/allow-black-queenside-castle? true
+           :fen/en-passant-target-square "e3"}
+        expected "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"]
+    (is (= expected (fen/map->fen m)))))
+
 (deftest after-1-e4-c5-fen-to-map-test
   (let [after-1-e4-c5-fen "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
         {:fen/keys [board
@@ -120,6 +130,26 @@
     (is (true? allow-black-queenside-castle?))
     (is (= "c6" en-passant-target-square))))
 
+(deftest after-1-e4-c5-map-to-fen-test
+  (let [m {:fen/board [\r \n \b \q \k \b \n \r
+                       \p \p \- \p \p \p \p \p
+                       \- \- \- \- \- \- \- \-
+                       \- \- \p \- \- \- \- \-
+                       \- \- \- \- \P \- \- \-
+                       \- \- \- \- \- \- \- \-
+                       \P \P \P \P \- \P \P \P
+                       \R \N \B \Q \K \B \N \R]
+           :fen/side-to-move :white
+           :fen/fullmove-number 2
+           :fen/halfmove-clock 0
+           :fen/allow-white-kingside-castle? true
+           :fen/allow-white-queenside-castle? true
+           :fen/allow-black-kingside-castle? true
+           :fen/allow-black-queenside-castle? true
+           :fen/en-passant-target-square "c6"}
+        expected "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"]
+    (is (= expected (fen/map->fen m)))))
+
 (deftest after-1-e4-c5-2-Nf3-fen-to-map-test
   (let [after-1-e4-c5-2-Nf3-fen "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
         {:fen/keys [board
@@ -148,6 +178,26 @@
     (is (true? allow-black-kingside-castle?))
     (is (true? allow-black-queenside-castle?))
     (is (nil? en-passant-target-square))))
+
+(deftest after-1-e4-c5-2-Nf3-map-to-fen-test
+  (let [m {:fen/board [\r \n \b \q \k \b \n \r
+                       \p \p \- \p \p \p \p \p
+                       \- \- \- \- \- \- \- \-
+                       \- \- \p \- \- \- \- \-
+                       \- \- \- \- \P \- \- \-
+                       \- \- \- \- \- \N \- \-
+                       \P \P \P \P \- \P \P \P
+                       \R \N \B \Q \K \B \- \R]
+           :fen/side-to-move :black
+           :fen/fullmove-number 2
+           :fen/halfmove-clock 1
+           :fen/allow-white-kingside-castle? true
+           :fen/allow-white-queenside-castle? true
+           :fen/allow-black-kingside-castle? true
+           :fen/allow-black-queenside-castle? true
+           :fen/en-passant-target-square nil}
+        expected "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"]
+    (is (= expected (fen/map->fen m)))))
 
 (deftest white-cant-castle-kingside-test
   (let [fen "rnbqkbnr/pppp1ppp/8/4p3/7P/8/PPPPPPPR/RNBQKBN1 b Qkq - 1 2"
@@ -182,7 +232,7 @@
     (is (true? allow-black-kingside-castle?))
     (is (false? allow-black-queenside-castle?))))
 
-(deftest two-kings-two-pawns-test
+(deftest two-kings-two-pawns-fen-to-map-test
   (let [fen "4k3/8/8/8/8/8/4P3/4K3 w - - 5 39"
         {:fen/keys [board
                     allow-white-kingside-castle?
@@ -212,3 +262,23 @@
     (is (= 5 halfmove-clock))
 
     (is (nil? en-passant-target-square))))
+
+(deftest two-kings-two-pawns-map-to-fen-test
+  (let [m {:fen/board [\- \- \- \- \k \- \- \-
+                       \- \- \- \- \- \- \- \-
+                       \- \- \- \- \- \- \- \-
+                       \- \- \- \- \- \- \- \-
+                       \- \- \- \- \- \- \- \-
+                       \- \- \- \- \- \- \- \-
+                       \- \- \- \- \P \- \- \-
+                       \- \- \- \- \K \- \- \-]
+           :fen/side-to-move :white
+           :fen/fullmove-number 39
+           :fen/halfmove-clock 5
+           :fen/allow-white-kingside-castle? false
+           :fen/allow-white-queenside-castle? false
+           :fen/allow-black-kingside-castle? false
+           :fen/allow-black-queenside-castle? false
+           :fen/en-passant-target-square nil}
+        expected "4k3/8/8/8/8/8/4P3/4K3 w - - 5 39"]
+    (is (= expected (fen/map->fen m)))))
